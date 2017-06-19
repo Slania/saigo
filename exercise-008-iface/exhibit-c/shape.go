d470ed59f078f9bc8448cd2551d03bda8cfaaad1
Srivathsava Rangarajan <srangarajan@enova.com>
2017-06-18 21:17:01 -0500

    Interface generation implemented

--- exercise-008-iface/exhibit-c/shape.go
+++ exercise-008-iface/exhibit-c/shape.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"math"
+	"math/rand"
+	"time"
 )
 
 ////////////
@@ -64,10 +66,58 @@ func Efficiency(s Shape) {
 	fmt.Printf("Efficiency of a %s is %f\n", name, efficiency)
 }
 
+type FactoryShape struct {
+	name      string
+	area      float64
+	perimeter float64
+	Shape
+}
+
+func (factoryShape *FactoryShape) Name() string {
+	return factoryShape.name
+}
+
+func (factoryShape *FactoryShape) Perimeter() float64 {
+	return factoryShape.perimeter
+}
+
+func (factoryShape *FactoryShape) Area() float64 {
+	return factoryShape.area
+}
+
+func Build(name string, args ...float64) func() Shape {
+	return func() (shape Shape) {
+		var factoryShape FactoryShape
+		if args != nil {
+			if len(args) > 1 {
+				factoryShape = FactoryShape{name: name, area: args[0], perimeter: args[1]}
+			} else {
+				factoryShape = FactoryShape{name: name, area: args[0], perimeter: rand.Float64() * 10.0}
+			}
+		} else {
+			factoryShape = FactoryShape{name: name, area: rand.Float64() * 10.0, perimeter: rand.Float64() * 10.0}
+		}
+
+		shape = &factoryShape
+		return
+	}
+}
+
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	s := Square{side: 10.0}
 	Efficiency(&s)
 
 	c := Circle{radius: 10.0}
 	Efficiency(&c)
+
+	generateFoo := Build("foo")
+	for i := 1; i <= 10; i++ {
+		Efficiency(generateFoo())
+	}
+
+	generateBar := Build("bar")
+	for i := 1; i <= 10; i++ {
+		Efficiency(generateBar())
+	}
 }
